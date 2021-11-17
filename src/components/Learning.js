339ea679b86368ded14bb8b71d04a5eccfd06311
unknown <nguyenhuyquang306@gmail.com>
2021-11-17 23:48:31 +0700

    get started

---
+++ src/components/Learning.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+
+class Learning extends Component{
+    render(){
+        return <h1> Hello Quang </h1>
+    }
+}
+
+export default Learning
